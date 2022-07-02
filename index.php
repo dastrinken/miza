04da8bca001cce5fa61fdd207404d80bd4fd0f15
dastrinken <61236723+dastrinken@users.noreply.github.com>
2022-07-02 15:53:58 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="./favicon.ico">
+    <link rel="icon" type="image/png" href="./favicon_32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="./favicon_96.png" sizes="96x96">
+    <link rel="apple-touch-icon" sizes="180x180" href="./favicon_180.png">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
